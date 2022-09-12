ceaea6f884633717f70eb83aeee3d25f049223bb
Pedro Magalhães <49410762+nonvegan@users.noreply.github.com>
2022-09-12 21:02:13 +0100

    Updates

--- gen_reports_and_csvs.py
+++ gen_reports_and_csvs.py
@@ -7,11 +7,12 @@ log_to_df = JSONLogToDataFrame()
 logs = list(map(lambda x: x[5:-4], glob.glob("logs/*.log")))
 
 for unwanted_log in ["packet_filter", "reporter"]:
-	logs.remove(unwanted_log)
+    if(unwanted_log in logs):
+	    logs.remove(unwanted_log)
 
-for log in logs:
+for log in ["conn", "http"]:
     print(log)
     df = log_to_df.create_dataframe("logs/{}.log".format(log))
     df.to_csv("datasets/{}.csv".format(log))
-    profile = ProfileReport(df, title="Zeek {} report".format(log), minimal=True)
+    profile = ProfileReport(df, title="Zeek {} report".format(log), explorative=True)
     profile.to_file("reports/{}_report.html".format(log))
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+from os import path, mkdir
+from sys import getsizeof
+from glob import glob
+from warnings import simplefilter
+
 from zat.json_log_to_dataframe import JSONLogToDataFrame
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+import pandas as pd
+import dateutil.parser as dparser
 
+simplefilter(action="ignore", category=FutureWarning)
 log_to_df = JSONLogToDataFrame()
+
+skip_samples_n = 0
+n_days = 2
+
 
 def add_totals(df):
     df["total_pkts"] = df["orig_pkts"] + df["resp_pkts"]
@@ -10,153 +21,90 @@ def add_totals(df):
     df["total_ip_bytes"] = df["orig_ip_bytes"] + df["resp_ip_bytes"]
     return df
 
+
 def add_flow_count(df):
-    df['flow_count'] = 1
+    df["flow_count"] = 1
     return df
+
 
 def resample_sum(df, rate):
-    df = df.resample(rate).sum()
-    return df
+    return df.resample(rate).sum()
 
-def flow_count_trace(df, name):
-    return go.Scatter(x=df.index, y=df["flow_count"], name=name, mode="lines+markers")
 
-sample_interval = "10S"
+def size_in_megabytes(x):
+    return getsizeof(x) / (1024 * 1024)
 
-# conn.log
-conn_df = log_to_df.create_dataframe("logs/conn.log")
-conn_df = add_totals(conn_df)
-conn_df = add_flow_count(conn_df)
-conn_udp_df = resample_sum(conn_df.query("proto=='udp'"), sample_interval)
-conn_tcp_df = resample_sum(conn_df.query("proto=='tcp'"), sample_interval)
-conn_icmp_df = resample_sum(conn_df.query("proto=='icmp'"), sample_interval)
-conn_df = resample_sum(conn_df, sample_interval)
-conn_df = conn_df.iloc[1:] # Skip first samples
 
-# http.log
-http_df = log_to_df.create_dataframe("logs/http.log")
-http_df = add_flow_count(http_df)
-http_df = resample_sum(http_df, sample_interval)
-http_df = http_df.iloc[1:] # Skip first samples
+def log_to_df_print(filenames, i):
+    print("Loading {} to a dataframe {}/{}".format(filenames[i], i + 1, len(filenames)))
+    return log_to_df.create_dataframe(filenames[i])
 
-# dns.log
-dns_df = log_to_df.create_dataframe("logs/dns.log")
-dns_df = add_flow_count(dns_df)
-dns_df = resample_sum(dns_df, sample_interval)
-dns_df = dns_df.iloc[1:] # Skip first samples
 
-# ssl.log
-ssl_df = log_to_df.create_dataframe("logs/ssl.log")
-ssl_df = add_flow_count(ssl_df)
-ssl_df = resample_sum(ssl_df, sample_interval)
-ssl_df = ssl_df.iloc[1:] # Skip first samples
+def start_date(first_log_filename):
+    # datetime.date(year=2022,month=6,day=1)
+    return dparser.parse(first_log_filename, fuzzy=True, dayfirst=False)
 
-# dce_rpc.log
-dce_rpc_df = log_to_df.create_dataframe("logs/dce_rpc.log")
-dce_rpc_df = add_flow_count(dce_rpc_df)
-dce_rpc_df = resample_sum(dce_rpc_df, sample_interval)
-dce_rpc_df = dce_rpc_df.iloc[1:] # Skip first samples
 
-# kerberos.log
-kerberos_df = log_to_df.create_dataframe("logs/kerberos.log")
-kerberos_df = add_flow_count(kerberos_df)
-kerberos_df = resample_sum(kerberos_df, sample_interval)
-kerberos_df = kerberos_df.iloc[1:] # Skip first samples
-
-def fig_apply_general_template():
-    global fig
+def fig_apply_general_template(fig):
     fig.update_xaxes(showgrid=False, zeroline=False)
     fig.update_yaxes(showgrid=False, zeroline=False)
     fig.update_layout(
-            hovermode='x unified',
-            title={'x':0.5, 'xanchor': 'center', 'yanchor': 'top'},
-            font=dict( size=18, color="RebeccaPurple"),
-            yaxis=dict( autorange = True, fixedrange= False),
-            xaxis=dict(
-                rangeselector=dict(
-                    buttons=list([
-                        dict(count=1,
-                            label="1m",
-                            step="minute",
-                            stepmode="backward"),
-                        dict(count=2,
-                            label="2m",
-                            step="minute",
-                            stepmode="backward"),
-                        dict(step="all")])),
-                    rangeslider=dict(visible=False)))
+        hovermode="x unified",
+        title={"x": 0.5, "xanchor": "center", "yanchor": "top"},
+        font=dict(size=18, color="RebeccaPurple"),
+        yaxis=dict(autorange=True, fixedrange=False),
+        xaxis=dict(rangeslider=dict(visible=False)),
+    )
 
-# Zeek Flows
-fig = go.Figure()
-fig.update_layout(title={ 'text': "SONAE Zeek logs"},
-                  xaxis_title="Date & Time ({} interval)".format(sample_interval.lower()),
-                  yaxis_title="Number of logs",
-                  legend_title="Zeek file")
-fig_apply_general_template()
-fig.add_trace(flow_count_trace(conn_df, "conn.log"))
-fig.add_trace(flow_count_trace(http_df, "http.log"))
-fig.add_trace(flow_count_trace(dns_df,  "dns.log"))
-fig.add_trace(flow_count_trace(ssl_df,  "ssl.log"))
-fig.add_trace(flow_count_trace(dce_rpc_df, "dce_rpc.log"))
-fig.add_trace(flow_count_trace(kerberos_df, "kerberos.log"))
-fig.show()
-fig.write_html("zeek_logs_dash.html")
 
-# Zeek Connection Packets
-fig = go.Figure()
-fig.update_layout(title={ 'text': "SONAE Connection Packets"},
-                  xaxis_title="Date & Time ({} interval)".format(sample_interval.lower()),
-                  yaxis_title="Number of Packets",
-                  legend_title="Packets")
-fig_apply_general_template()
-fig.add_trace(go.Scatter(x=conn_df.index, y=conn_df["total_pkts"], name="Total"))
-fig.add_trace(go.Scatter(x=conn_df.index, y=conn_df["orig_pkts"], name="Origin"))
-fig.add_trace(go.Scatter(x=conn_df.index, y=conn_df["resp_pkts"], name="Response"))
-fig.for_each_trace(lambda trace: trace.update(mode="lines", hoverinfo="name+x+y"))
-fig.show()
-fig.write_html("zeek_packets_dash.html")
+# conn.log
+conn_files = glob("logs/conn*.log")[0:n_days]
+conn_df = pd.DataFrame()
+for i in range(len(conn_files)):
+    conn_file = conn_files[i]
+    sub_conn_df = log_to_df_print(conn_files, i)
+    sub_conn_df = add_totals(sub_conn_df)
+    sub_conn_df = add_flow_count(sub_conn_df)
+    # sub_conn_df = resample_sum(sub_conn_df, sample_interval)
+    conn_df = pd.concat([conn_df, sub_conn_df])
+print("Size of Conn DF -> {}MB".format(size_in_megabytes(conn_df)))
+conn_df = resample_sum(conn_df, "1min")
+conn_df = conn_df.iloc[skip_samples_n:]
+conn_df = conn_df.loc[start_date(conn_files[0]) :]
+print("Size of Resampled Conn DF -> {}MB\n".format(size_in_megabytes(conn_df)))
 
-# Zeek Connection bytes
-fig = go.Figure()
-fig.update_layout(title={ 'text': "SONAE Connection Bytes"},
-                  xaxis_title="Date & Time ({} interval)".format(sample_interval.lower()),
-                  yaxis_title="Number of Bytes",
-                  legend_title="Bytes")
-fig_apply_general_template()
-fig.add_trace(go.Scatter(x=conn_df.index, y=conn_df["total_bytes"], name="Payload Total"))
-fig.add_trace(go.Scatter(x=conn_df.index, y=conn_df["orig_bytes"], name="Payload Origin"))
-fig.add_trace(go.Scatter(x=conn_df.index, y=conn_df["resp_bytes"], name="Payload Response"))
-fig.add_trace(go.Scatter(x=conn_df.index, y=conn_df["total_ip_bytes"], name="IP Total"))
-fig.add_trace(go.Scatter(x=conn_df.index, y=conn_df["orig_ip_bytes"], name="IP Origin"))
-fig.add_trace(go.Scatter(x=conn_df.index, y=conn_df["resp_ip_bytes"], name="IP Response"))
-fig.for_each_trace(lambda trace: trace.update(mode="lines", hoverinfo="name+x+y"))
-fig.show()
-fig.write_html("zeek_bytes_dash.html")
+conn_metric_intervals = ["30min", "1hour", "1day"]
+conn_metrics_to_report = ["flow_count", "duration"]
 
-# Zeek Connection by protocol
-fig = go.Figure()
-fig.update_layout(title={ 'text': "SONAE Connection Protocols"},
-                  xaxis_title="Date & Time ({} interval)".format(sample_interval.lower()),
-                  yaxis_title="Number of Connections",
-                  legend_title="Protocol")
-fig_apply_general_template()
-fig.add_trace(go.Scatter(x=conn_udp_df.index, y=conn_udp_df["flow_count"], marker=dict(color='#636efa'), text=conn_udp_df["flow_count"], name="udp"))
-fig.add_trace(go.Scatter(x=conn_tcp_df.index, y=conn_tcp_df["flow_count"], marker=dict(color='#ef553b'), text=conn_tcp_df["flow_count"], name="tcp"))
-fig.add_trace(go.Scatter(x=conn_icmp_df.index, y=conn_icmp_df["flow_count"], marker=dict(color='#00cc96'), text=conn_icmp_df["flow_count"], name="icmp"))
-fig.for_each_trace(lambda trace: trace.update(mode='lines+text+markers', textfont_color=trace.marker.color, textposition='top center', hoverinfo="name+x+y"))
-fig.show()
-fig.write_html("zeek_proto_dash.html")
+if not path.exists("./metrics"):
+    mkdir("metrics")
 
-fig = make_subplots(specs=[[{"secondary_y": True}]])
-fig.update_layout(title={ 'text': "SONAE Avg Connection Duration"},
-                  xaxis_title="Date & Time ({} interval)".format(sample_interval.lower()),
-                  yaxis_title="Number of packets",
-                  yaxis2_title="Avg. Duration (s)",
-                  legend_title="Protocol")
-fig_apply_general_template()
-fig.add_trace(go.Scatter(x=conn_udp_df.index, y=conn_df["duration"] / conn_df["flow_count"], name="Duration", mode='lines'), secondary_y=True)
-fig.add_trace(go.Bar(x=conn_udp_df.index, y=conn_df["orig_pkts"], name="Origin Packets"))
-fig.add_trace(go.Bar(x=conn_udp_df.index, y=conn_df["resp_pkts"], name="Response Packets"))
-fig.update_layout(barmode='stack')
-fig.show()
-fig.write_html("zeek_conn_packets_duration_dash.html")
+
+for metric_interval in conn_metric_intervals:
+    conn_df = resample_sum(conn_df, pd.Timedelta(metric_interval))
+    for metric in conn_metrics_to_report:
+        id_min = conn_df[metric].idxmin()
+        id_max = conn_df[metric].idxmax()
+        print(
+            "{}/{}: Avg: {}, Min: {} ({}), Max: {} ({})".format(
+                metric,
+                metric_interval,
+                conn_df[metric].mean(),
+                conn_df[metric][id_min],
+                id_min,
+                conn_df[metric][id_max],
+                id_max,
+            )
+        )
+        fig = go.Figure()
+        fig.update_layout(
+            title={"text": "Connection Sum({})/{}".format(metric, metric_interval)},
+            xaxis_title="timestamp",
+            yaxis_title=metric,
+        )
+        fig_apply_general_template(fig)
+        fig.add_trace(go.Scatter(x=conn_df.index, y=conn_df[metric]))
+        fig.write_html("metrics/{}_{}_{}.html".format("conn", metric, metric_interval))
+
+    print("")
+# conn_df.to_csv("conn_df.csv")
