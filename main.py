fa6ab2c242887fb50119d53ffd0b10c169ed8b86
Pedro Magalhães <49410762+nonvegan@users.noreply.github.com>
2022-09-13 21:40:23 +0100

    Tidy up functions and path gen

--- main.py
+++ main.py
@@ -67,6 +67,54 @@ def fig_apply_general_template(fig):
     )
 
 
+def write_metric_figure(df, zeek_file, metric_aggr, metric, metric_interval, base_path):
+    fig = go.Figure()
+    fig.update_layout(
+        title={
+            "text": "{} {}({})/{}".format(
+                zeek_file, metric_aggr, metric, metric_interval
+            )
+        },
+        xaxis_title="timestamp",
+        yaxis_title=metric,
+    )
+    fig_apply_general_template(fig)
+    fig.add_trace(go.Scatter(x=df.index, y=df[metric]))
+
+    if not path.exists("{}/{}/{}".format(base_path, metric_aggr, metric)):
+        makedirs("{}/{}/{}".format(base_path, metric_aggr, metric))
+
+    fig.write_html(
+        "{}/{}/{}/{}_{}_{}_{}.html".format(
+            base_path,
+            metric_aggr,
+            metric,
+            zeek_file,
+            metric_aggr,
+            metric,
+            metric_interval,
+        )
+    )
+
+
+def print_metric_info(df, zeek_file, metric_aggr, metric, metric_interval):
+    id_min = df[metric].idxmin()
+    id_max = df[metric].idxmax()
+    print(
+        "{} {}({})/{}: Avg: {}, Min: {} ({}), Max: {} ({})\n".format(
+            zeek_file,
+            metric_aggr,
+            metric,
+            metric_interval,
+            df[metric].mean(),
+            df[metric][id_min],
+            id_min,
+            df[metric][id_max],
+            id_max,
+        )
+    )
+
+
 # conn.log
 n_days = 7
 conn_metric_intervals = ["30min", "1H", "24H"]
@@ -115,49 +163,6 @@ conn_df = resample_sum(conn_df, "1min").loc[log_date(conn_filenames[0]) :]
 print("Size of Resampled Conn DF -> {}MB\n".format(size_in_megabytes(conn_df)))
 # conn_df.to_csv("conn_df.csv")
 
-
-def write_metric_figure(df, zeek_file, metric_aggr, metric, metric_interval):
-    fig = go.Figure()
-    fig.update_layout(
-        title={
-            "text": "{} {}({})/{}".format(
-                zeek_file, metric_aggr, metric, metric_interval
-            )
-        },
-        xaxis_title="timestamp",
-        yaxis_title=metric,
-    )
-    fig_apply_general_template(fig)
-    fig.add_trace(go.Scatter(x=df.index, y=df[metric]))
-
-    if not path.exists("./metrics/{}/{}".format(metric_aggr, metric)):
-        makedirs("./metrics/{}/{}".format(metric_aggr, metric))
-
-    fig.write_html(
-        "metrics/{}/{}/{}_{}_{}_{}.html".format(
-            metric_aggr, metric, zeek_file, metric_aggr, metric, metric_interval
-        )
-    )
-
-
-def print_metric_info(df, zeek_file, metric_aggr, metric, metric_interval):
-    id_min = df[metric].idxmin()
-    id_max = df[metric].idxmax()
-    print(
-        "{} {}({})/{}: Avg: {}, Min: {} ({}), Max: {} ({})\n".format(
-            zeek_file,
-            metric_aggr,
-            metric,
-            metric_interval,
-            df[metric].mean(),
-            df[metric][id_min],
-            id_min,
-            df[metric][id_max],
-            id_max,
-        )
-    )
-
-
 if path.exists("./metrics"):
     rmtree("./metrics")
 mkdir("./metrics")
@@ -166,7 +171,9 @@ for metric_interval in conn_metric_intervals:
     conn_df = resample_sum(conn_df, pd.Timedelta(metric_interval))
     for conn_metric_to_sum in conn_metrics_to_sum:
         print_metric_info(conn_df, "conn", "sum", conn_metric_to_sum, metric_interval)
-        write_metric_figure(conn_df, "conn", "sum", conn_metric_to_sum, metric_interval)
+        write_metric_figure(
+            conn_df, "conn", "sum", conn_metric_to_sum, metric_interval, "./metrics"
+        )
 
 for metric_interval in conn_metric_intervals:
     resampled_conn_df_uniques = resample_uniq(conn_df_uniques, metric_interval).loc[
@@ -186,4 +193,5 @@ for metric_interval in conn_metric_intervals:
             "unique",
             conn_metric_to_count_uniques,
             metric_interval,
+            "./metrics",
         )
