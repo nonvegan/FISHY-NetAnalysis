819d71a43c9653a95f6d26147ca26b53ed369dd4
nonvegan <pedromagalhaes_2000@hotmail.com>
2022-09-13 16:54:56 +0100

    Unique analysis

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from os import path, mkdir
+from shutil import rmtree
 from sys import getsizeof
 from glob import glob
 from warnings import simplefilter
 
 from zat.json_log_to_dataframe import JSONLogToDataFrame
+
 import plotly.graph_objects as go
 import pandas as pd
 import dateutil.parser as dparser
@@ -13,7 +15,6 @@ log_to_df = JSONLogToDataFrame()
 
 skip_samples_n = 0
 n_days = 2
-
 
 def add_totals(df):
     df["total_pkts"] = df["orig_pkts"] + df["resp_pkts"]
@@ -35,91 +36,100 @@ def size_in_megabytes(x):
     return getsizeof(x) / (1024 * 1024)
 
 
-def log_to_df_print(filenames, i):
-    print("Loading {} to a dataframe {}/{}".format(filenames[i], i + 1, len(filenames)))
-    df = log_to_df.create_dataframe(filenames[i])
-    for col in [
-        "uid",
-        "conn_state",
-        "history",
-        "service",
-        "proto",
-        "orig_l2_addr",
-        "resp_l2_addr",
-    ]:
-        df.drop(col, axis=1, inplace=True)
+def log_to_df_print(filename):
+    global conn_filenames_len
+    global conn_filenames_index
+    print("Loading {} to a dataframe {}/{}".format(filename, conn_filenames_index + 1, conn_filenames_len))
+    return log_to_df.create_dataframe(filename)
 
 
-def start_date(first_log_filename):
+def log_date(log_filename):
     # datetime.date(year=2022,month=6,day=1)
-    return dparser.parse(first_log_filename, fuzzy=True, dayfirst=False)
+    return dparser.parse(log_filename, fuzzy=True, dayfirst=False)
 
 
 def fig_apply_general_template(fig):
     fig.update_xaxes(showgrid=False, zeroline=False)
     fig.update_yaxes(showgrid=False, zeroline=False)
     fig.update_layout(
-        hovermode="x unified",
-        title={"x": 0.5, "xanchor": "center", "yanchor": "top"},
-        font=dict(size=18, color="RebeccaPurple"),
-        yaxis=dict(autorange=True, fixedrange=False),
-        xaxis=dict(rangeslider=dict(visible=False)),
-    )
+            hovermode="x unified",
+            title={"x": 0.5, "xanchor": "center", "yanchor": "top"},
+            font=dict(size=18, color="RebeccaPurple"),
+            yaxis=dict(autorange=True, fixedrange=False),
+            xaxis=dict(rangeslider=dict(visible=False)),
+            )
 
 
 # conn.log
-conn_files = glob("logs/conn*.log")[0:n_days]
+conn_metric_intervals = ["30min", "1hour", "1day"]
+conn_metrics_to_sum = ["flow_count", "duration"]
+conn_metrics_to_count_unique = ["id.orig_h", "id.resp_h", "id.orig_p", "id.resp_p", "orig_l2_addr", "resp_l2_addr"]
+
+conn_filenames = sorted(glob("logs/conn*.log"))[0:n_days]
+conn_filenames_len = len(conn_filenames)
+
 conn_df = pd.DataFrame()
-for i in range(len(conn_files)):
-    conn_file = conn_files[i]
-    sub_conn_df = log_to_df_print(conn_files, i)
+conn_df_uniques = pd.DataFrame()
+
+for i in range(len(conn_filenames)):
+    conn_filenames_index = i
+    sub_conn_df = log_to_df_print(conn_filenames[i])
     sub_conn_df = add_totals(sub_conn_df)
     sub_conn_df = add_flow_count(sub_conn_df)
-    # sub_conn_df = resample_sum(sub_conn_df, sample_interval)
+
+    for col in list(sub_conn_df):
+        if col not in conn_metrics_to_sum + conn_metrics_to_count_unique:
+            sub_conn_df.drop(col, axis=1, inplace=True)
+
+    conn_df_uniques = pd.concat([conn_df_uniques, sub_conn_df[conn_metrics_to_count_unique]])
+    sub_conn_df.drop(conn_metrics_to_count_unique, inplace=True, axis=1)
+
     conn_df = pd.concat([conn_df, sub_conn_df])
 
 print(conn_df.info())
-
+print(list(conn_df))
 print("Size of Conn DF -> {}MB".format(size_in_megabytes(conn_df)))
+
+print(conn_df_uniques.info())
+print(list(conn_df_uniques))
+print("Size of Conn DF Unique -> {}MB".format(size_in_megabytes(conn_df_uniques)))
+
 conn_df = resample_sum(conn_df, "1min")
 conn_df = conn_df.iloc[skip_samples_n:]
-conn_df = conn_df.loc[start_date(conn_files[0]) :]
+conn_df = conn_df.loc[log_date(conn_filenames[0]) :]
 print("Size of Resampled Conn DF -> {}MB\n".format(size_in_megabytes(conn_df)))
 
 #conn_df.to_csv("conn_df.csv")
 
-conn_metric_intervals = ["30min", "1hour", "1day"]
-conn_metrics_to_report = ["flow_count", "duration"]
-
-if not path.exists("./metrics"):
-    mkdir("metrics")
-
+if path.exists("./metrics"):
+    rmtree("./metrics");
+mkdir("./metrics")
 
 for metric_interval in conn_metric_intervals:
     conn_df = resample_sum(conn_df, pd.Timedelta(metric_interval))
-    for metric in conn_metrics_to_report:
+    for metric in conn_metrics_to_sum:
         id_min = conn_df[metric].idxmin()
         id_max = conn_df[metric].idxmax()
         print(
-            "{}/{}: Avg: {}, Min: {} ({}), Max: {} ({})".format(
-                metric,
-                metric_interval,
-                conn_df[metric].mean(),
-                conn_df[metric][id_min],
-                id_min,
-                conn_df[metric][id_max],
-                id_max,
-            )
-        )
+                "{}/{}: Avg: {}, Min: {} ({}), Max: {} ({})".format(
+                    metric,
+                    metric_interval,
+                    conn_df[metric].mean(),
+                    conn_df[metric][id_min],
+                    id_min,
+                    conn_df[metric][id_max],
+                    id_max,
+                    )
+                )
         fig = go.Figure()
         fig.update_layout(
-            title={"text": "Connection Sum({})/{}".format(metric, metric_interval)},
-            xaxis_title="timestamp",
-            yaxis_title=metric,
-        )
+                title={"text": "Connection Sum({})/{}".format(metric, metric_interval)},
+                xaxis_title="timestamp",
+                yaxis_title=metric,
+                )
         fig_apply_general_template(fig)
         fig.add_trace(go.Scatter(x=conn_df.index, y=conn_df[metric]))
-        fig.write_html("metrics/{}_{}_{}.html".format("conn", metric, metric_interval))
+        fig.write_html("metrics/{}_{}_{}_{}.html".format("conn", "sum", metric, metric_interval))
 
     print("")
 # conn_df.to_csv("conn_df.csv")
